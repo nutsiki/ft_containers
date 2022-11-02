f29c13b3f076b23609e2e1c6fdaf049abbe560aa
moakouda <mo.akoudad@gmail.com>
2022-11-02 14:15:54 +0100

    push header

--- vector.hpp
+++ vector.hpp
@@ -1,23 +1,27 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+# include <memory>
+
 namespace ft
 {
-    template < class T, class Alloc = allocator<T> > class vector;
+    template < class T, class Alloc = std::allocator<T> >
     class vector 
     {
 
     // Member types
-    typedef value_type = T;
-    typedef allocator_type = Allocator;
-    typedef size_type = std::size_t;
-    typedef difference_type = std::ptrdiff_t;
-    typedef reference = value_type&;
-    typedef const_reference = const value_type&;
-    typedef pointer = Allocator::pointer;
-    typedef const_pointer = Allocator::const_pointer;
-    typedef iterator ;
-    typedef const_iterator ;
-    typedef reverse_iterator;
-    typedef const_reverse_iterator;
-
+    typedef T value_type ;
+    typedef Alloc allocator_type;
+    typedef typename allocator_type::reference reference;
+    typedef typename allocator_type::const_reference const_reference;
+    typedef typename allocator_type::pointer pointer;
+    typedef typename allocator_type::const_pointer const_pointer;
+    typedef ft::iterator<value_type> iterator ;
+    typedef ft::iterator<const value_type> const_iterator ;
+    typedef ft::reverse_iterator<iterator> reverse_iterator;
+    typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
+    typedef typename iterator_traits<iterator>::difference_type difference_type;
+    typedef std::size_t size_type;
+    
     // Member fonctions
     vector();
     ~vector();
@@ -77,3 +81,5 @@ namespace ft
     template <class T, class Alloc>  void swap (vector<T,Alloc>& x, vector<T,Alloc>& y);
     } 
 }
+
+#endif
